f416c97befd0110b8312f0a1cd7324ce63030fa9
yohanesrinto <yohanes.tennary@ti.ukdw.ac.id>
2022-03-06 21:38:52 +0700

    Temp tugas pertemuan 5

---
+++ pertemuan5_71180259/app/src/main/java/com/rinto/pertemuan5_71180259/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rinto.pertemuan5_71180259
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
